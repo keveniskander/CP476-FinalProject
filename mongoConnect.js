06eb22b776baad1113a04bb38e5edabb86e3e157
keveniskander <keven.iskander@hotmail.com>
2021-04-08 19:22:15 -0400

    mongoConnect.js changed directory

--- mongoConnect.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:1234@projects.rorbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("sudoku").collection("users");
-  // perform actions on the collection object
-  client.close();
-});
