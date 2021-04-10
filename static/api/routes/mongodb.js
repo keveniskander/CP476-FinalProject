370915612a8f426649ee335e120f85d56b233bea
carlaac99 <cast4730@mylaurier.ca>
2021-04-09 22:06:48 -0400

    top 5 table working

--- static/api/routes/mongodb.js
+++ static/api/routes/mongodb.js
@@ -19,6 +19,37 @@ router.get("/", function(req, res, next) {
     console.log('test1');
     res.send("API is working properly");
 
+
+//     var MongoClient = require('mongodb').MongoClient;
+
+//     var url = "mongodb+srv://admin:1234@projects.rorbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//     MongoClient.connect(url, function(err, db) {
+
+//     if (err) throw err;
+//     var dbo = db.db("sudoku");
+//     // var myobj = { name: "test", address: "time" };
+
+//     dbo.collection('users').find({}).toArray(function(err,result){
+//         if (err) throw err;
+        
+//         console.log(result)
+//         res.send("fdsfsfd");
+        
+//         db.close();
+        
+
+//     });
+    
+
+
+//     console.log('test1');
+    
+
+    
+
+// });
+
+
 });
 
 
@@ -28,7 +59,6 @@ router.get("/users", function(req, res, next) {
 
     var url = "mongodb+srv://admin:1234@projects.rorbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     MongoClient.connect(url, function(err, db) {
-        dbo.foo.find().sort({_id:1}).limit(50);
 
     if (err) throw err;
     var dbo = db.db("sudoku");
@@ -36,17 +66,21 @@ router.get("/users", function(req, res, next) {
 
     dbo.collection('users').find({}).sort({_id:1}).limit(5).toArray(function(err,result){
         if (err) throw err;
-        res.send(JSON.stringify(result));
+        
         console.log(result)
+        res.send(JSON.stringify(result));
         
         db.close();
+        
 
     });
     
 
 
     console.log('test1');
-    res.send("API is working properly");
+    
+
+    
 
 });
 
