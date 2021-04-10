d8c8f7c0e87ebb563996583b08df479a5dc3365b
keveniskander <keven.iskander@hotmail.com>
2021-04-09 21:43:37 -0400

    testing api

--- static/api/routes/mongodb.js
+++ static/api/routes/mongodb.js
@@ -22,33 +22,33 @@ router.get("/", function(req, res, next) {
 });
 
 
-router.get("/users", function(req, res, next) {
+// router.get("/users", function(req, res, next) {
 
-    var MongoClient = require('mongodb').MongoClient;
+//     var MongoClient = require('mongodb').MongoClient;
 
-    var url = "mongodb+srv://admin:1234@projects.rorbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    MongoClient.connect(url, function(err, db) {
-        dbo.collection("users").find().sort({_id:1}).limit(50);
+//     var url = "mongodb+srv://admin:1234@projects.rorbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//     MongoClient.connect(url, function(err, db) {
+//         dbo.collection("users").find().sort({_id:1}).limit(50);
 
-    if (err) throw err;
-    var dbo = db.db("sudoku");
-    // var myobj = { name: "test", address: "time" };
+//     if (err) throw err;
+//     var dbo = db.db("sudoku");
+//     // var myobj = { name: "test", address: "time" };
 
-    dbo.collection('users').find({}).sort({_id:1}).limit(5).toArray(function(err,result){
-        if (err) throw err;
-        res.send(JSON.stringify(result));
-        console.log(result)
+//     dbo.collection('users').find({}).sort({_id:1}).limit(5).toArray(function(err,result){
+//         if (err) throw err;
+//         res.send(JSON.stringify(result));
+//         console.log(result)
         
-        db.close();
+//         db.close();
 
-    });
+//     });
     
 
 
-    console.log('test1');
-    res.send("API is working properly");
+//     console.log('test1');
+//     res.send("API is working properly");
 
-});
+// });
 
-});
+// });
 module.exports = router;
